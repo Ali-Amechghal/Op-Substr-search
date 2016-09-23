e7086652c9468dea947a2f6a75dae8836739bb60
Ali-Amechghal <ali.amechghal@gmail.com>
2016-09-23 23:47:56 +0100

    init

---
+++ search.js
@@ -0,0 +1,9 @@
+exports.substring_search = function(substrings, text) {
+  "use strict";
+  var results = {};
+  var textLength = text.length;
+  var substringsLength = substrings.length;
+  var brute = false;
+ 
+  return results;
+};
